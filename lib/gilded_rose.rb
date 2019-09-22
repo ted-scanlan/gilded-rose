ef759b2c186dac66b230aae1889ce4f0dfc3e90c
ted_scanlan <t.scanlan92@gmail.com>
2019-09-22 23:38:38 +0100

    tidied up the update quality method with some if else statements depending on the type of item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,56 +4,40 @@ class GildedRose
     @items = items
   end
 
-
-
   def update_quality()
     @items.each do |item|
 
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+        return if item.name == "Sulfuras, Hand of Ragnaros"
+        return if item.quality == 0
+        item.sell_in -= 1
+
+        if item.name == "Aged Brie"
+          return if item.quality >= 50
+          if item.sell_in < 0
+            item.quality += 2
           else
-            item.quality = item.quality - item.quality
+            item.quality += 1
+          end
+
+        elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.sell_in <= 0
+            item.quality = 0
+          elsif item.sell_in <= 5
+              item.quality += 3
+          elsif item.sell_in <= 10
+              item.quality += 2
+          else
+              item.quality += 1
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
+          if item.sell_in <= 0
+            item.quality -= 2
+          else
+            item.quality -= 1
           end
         end
-      end
-    end
   end
+end
 end
 
 class Item
