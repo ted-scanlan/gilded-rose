47e4d976055e0850600e357760f5c880dba0ddcc
ted_scanlan <t.scanlan92@gmail.com>
2019-09-24 15:01:33 +0100

    fixed backstage passes bug

--- lib/Aged_brie.rb
+++ lib/Aged_brie.rb
@@ -3,8 +3,8 @@ require_relative './Item'
 class AgedBrie < Item
 
   def update
-    return if @quality >= 50
     @sell_in -= 1
+    return if @quality >= 50
     @sell_in < 0 ? @quality += 2 : @quality += 1
 
   end
--- lib/Backstage_passes.rb
+++ lib/Backstage_passes.rb
@@ -1,16 +1,18 @@
 require_relative './Item'
 
 class BackstagePasses < Item
+  MAX = 50
 
   def update
     @sell_in -= 1
+    return if @quality >= 50
     case @sell_in
     when -1..0
       @quality = 0
     when 1..5
-      @quality += 3
+      @quality == 48 || @quality == 49 ? @quality = MAX : @quality += 3
     when 6..10
-      @quality += 2
+      @quality == 49 ? @quality = MAX : @quality += 2
     else
       @quality += 1
     end
